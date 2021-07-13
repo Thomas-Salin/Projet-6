bfc183373a7808d8e0d2c9c40c50339975de7dcf
Thomas Salin <salin.thomas.pro@gmail.com>
2021-07-13 20:52:06 +0200

    Integration Routes "User, Commentaire, gif" - integration controllers POST

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,22 @@
 const express = require('express');
 const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const gifRoutes = require('./routes/gif');
 
 const app = express();
 
 
-app.get('/api/gifs', (req, res, next) => {
+app.use(bodyParser.json());
 
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'groupomania',
-        password: 'groupomania',
-        database: 'reseau_groupomania'
-      });
-      
-      connection.promise().query(`SELECT gif_url, description, DATE_FORMAT(date_heure_publi, '%e/%c/%Y à %kh%i'), Utilisateur.prenom AS prenom, Utilisateur.nom AS nom FROM Gif INNER JOIN Utilisateur ON Gif.utilisateur_id = Utilisateur.id`) 
-        .then ( ([rows, fields]) => {    
-          let response = Object.values(rows);
-          return res.status(200).json({response});
-        })
-        .catch(() => res.status(500).json({error: 'Connexion au serveur impossible'}))
-        .then (() => connection.end());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
-app.get('/api/gifs/:id', (req, res, next) => {
+app.use('/api/gifs', gifRoutes);
 
-  let id = req.params.id;
-
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'groupomania',
-    password: 'groupomania',
-    database: 'reseau_groupomania'
-  });
-
-  connection.promise().query(`SELECT commentaire, utilisateur.prenom AS prenom_commentaire, utilisateur.nom AS nom_commentaire, DATE_FORMAT(date_heure_publication, '%e/%c/%Y à %kh%i') AS date_commentaire, gif, gif_auteurprenom, gif_auteurnom, gif_date FROM commentaire INNER JOIN (SELECT gif.id AS gif_id, gif.gif_url AS gif, utilisateur.prenom AS gif_auteurprenom, utilisateur.nom AS gif_auteurnom, DATE_FORMAT(date_heure_publi, '%e/%c/%Y à %kh%i') AS gif_date FROM Gif INNER JOIN Utilisateur ON gif.utilisateur_id = Utilisateur.id) AS req ON commentaire.gif_id = req.gif_id INNER JOIN Utilisateur ON commentaire.utilisateur_id = Utilisateur.id WHERE req.gif_id = ? `, [id] )
-    .then ( ([rows,fields]) => {
-      let response = Object.values(rows);
-      return res.status(200).json({response});
-    })
-    .catch ( () => res.status(500).json({error :'Connexion au serveur impossible'}))
-    .then ( () => connection.end());
-
-});
 
 module.exports = app;
